94c96378cc6b8c9e8417e508887b31f7a1a0085d
Alfrehan <a.hannzel@gmail.com>
2021-07-12 19:51:10 -0300

    Primeiro projeto Android

---
+++ app/src/main/java/br/com/senac/transitaractivity/MainActivity.java
@@ -0,0 +1,17 @@
+package br.com.senac.transitaractivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.act)
+    }
+}
